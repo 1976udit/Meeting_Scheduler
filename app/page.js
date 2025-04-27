d52c7971e4cfc8ae739a338c850322b26c1fc956
1975udit <udit2115418@akgec.ac.in>
2025-04-27 15:28:27 +0530

    Header

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Header from './_components/Header';
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-     <Button className="bg-blue-500 text-white hover:bg-blue-700">Click me</Button>
+      <Header />
     </div>
   );
 }
