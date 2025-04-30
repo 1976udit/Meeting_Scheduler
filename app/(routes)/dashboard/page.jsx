e5885ddf8506a63572bc2b9973b46cf806100049
1975udit <udit2115418@akgec.ac.in>
2025-04-30 23:32:07 +0530

    dashboard

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,8 +1,34 @@
 "use client"
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import { app } from '../../../config/firebase.Config'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { doc, getDoc ,getFirestore } from 'firebase/firestore'
+import React, { useEffect } from 'react'
 
 const Dashboard = () => {
+
+  const db = getFirestore(app)
+  const {user} = useKindeBrowserClient()
+  // console.log(user)
+
+  const isBussinessRegistered = async () => {
+    try {
+      const docRef = doc(db, "BUSINESS", user?.email);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error fetching document:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    user && isBussinessRegistered();
+  }, [user]);
+
   return (
     <div>
         <h1>Dashboard</h1>
